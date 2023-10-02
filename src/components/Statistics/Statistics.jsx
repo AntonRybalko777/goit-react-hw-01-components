fd948ed18f6e86fe4d53b7b0576163fb8a735091
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-10-02 13:36:17 +0300

    fixed title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { getRandomHexColor } from 'get-random-color';
 export const Statistics = ({ stats, title }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(stat => (
           <li
